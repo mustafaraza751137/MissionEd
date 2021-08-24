c884656f015018e744f1bba2e6884f151a1e3dd1
Mustafa Raza <73385368+mustafaraza751137@users.noreply.github.com>
2021-08-24 12:20:55 +0530

    .

--- src/StoreSection/StoreSection.js
+++ src/StoreSection/StoreSection.js
@@ -14,7 +14,7 @@ const StoreSection = (props) => {
                 </div>
             </div>
             <div className={classes.image}>
-                <img src="https://github.com/mustafaraza751137/MissionEd/blob/main/src/Images/fluid.png?raw=true" alt="fluid"/>
+                <img src="https://github.com/mustafaraza751137/MissionEd/blob/main/src/Images/fluid.jpg?raw=true" alt="fluid"/>
             </div>
         </div>
     );
