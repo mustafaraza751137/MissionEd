15da2763c5f6d622d2a5a91950f1064ff9ba7807
Mustafa Raza <73385368+mustafaraza751137@users.noreply.github.com>
2021-08-24 13:12:27 +0530

    image update

--- src/ClassSection/ClassSection.js
+++ src/ClassSection/ClassSection.js
@@ -5,7 +5,7 @@ const ClassSection = (props) => {
     return(
         <div className={classes.container}>
             <div className={classes.content}>
-                <h1><img src="../Images/logo.png" alt="Content"/>MISSIONED CLASSES</h1>
+                <h1><img src="https://github.com/mustafaraza751137/MissionEd/blob/main/src/Images/logo.png?raw=true" alt="Content"/>MISSIONED CLASSES</h1>
                 <h2>Get the best classes by the best Faculty members in the Country</h2>
             </div>
             <div className={classes.image}>
